85200dc3d6d21ff3750faf725ad6b31b98323775
aoyama-n <57561847+aoyama-n@users.noreply.github.com>
2020-01-23 19:20:21 +0900

    BlogControllerにメソッドを追加

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -22,18 +22,58 @@ class BlogController extends Controller {
     //記事追加(POST登録処理)
     public function blog_add(Request $request) {
         //バリデーション
-        
+        $validatedData = $request->validate([
+            'title' => 'request|string|max:200',
+            'content' => 'required|string'
+        ]);
         // レコード登録処理
+        $post = new Post();
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->save();
+        return view('result');
         
     }
 
     //記事一覧
+    public function blog_list() {
+        $posts = POST::all();
+        return view('blog_list',['posts' => $posts]);
+    }
 
     //記事編集(フォーム表示)
+    public function blog_editForm($id) {
+        $posts = Post::find($id);
+        if(is_null($post)) {
+            return redirect('blog_add');
+        }
+        return view('blog_editForm', ['post' => $post]);
+    }
 
     //記事編集(POST登録処理)
+    public function blog_edit(Request $request){
+        $request->validate([
+            'id' => 'required',
+            'title' => 'required|string',
+            'content' => 'required|string'
+        ]);
+
+        $post = Post::find($request->id);
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->save();
+        return redirect('blog_list');
+    }
 
     //記事論理削除
+    public function blog_delete(Request $request){
+        $validatedData = $request->validate([
+            'ids' => 'array|required'
+        ]);
+
+        Post::destroy($request->ids);
+        return redirect('blog_list');
+    }
 
     //カテゴリー一覧
 

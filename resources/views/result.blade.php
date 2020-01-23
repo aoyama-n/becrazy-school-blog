cb2dfd78ba4d64eb94e95aca93ee1836f3b598db
aoyama-n <57561847+aoyama-n@users.noreply.github.com>
2020-01-23 19:16:36 +0900

    記事追加result表示のbladeを作成

---
+++ resources/views/result.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="jp">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>保存</title>
+
+    </head>
+    <body>
+        <h1>保存しました！</h1>
+        <a href="blog_add">戻る</a>
+    </body>
+</html>

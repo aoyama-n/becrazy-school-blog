bd3efdf68e56c6b93b2f3e49e615a0aa9b0a5774
aoyama-n <57561847+aoyama-n@users.noreply.github.com>
2020-01-24 20:46:45 +0900

    カテゴリーリストのbladeを作成

---
+++ resources/views/category_list.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="jp">
+
+<head>
+    <meta charset="utf-8">
+    <title>カテゴリーリスト</title>
+
+</head>
+<body>
+    <div class="flex-center position-ref">
+        <h1>ブログカテゴリーリスト</h1>
+
+        <form method="POST" action="category_delete">
+            <a href="category_add">カテゴリー追加</a>
+            @csrf
+            <table border="1" cellspacing="0" cellpadding="5" align="left">
+                <thead>
+                    <tr>
+                        <th>選択</th><th>id</th><th>名前</th><th>タイプ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($taxonomys as $taxonomy)
+                        <tr>
+                            <td><input type="checkbox" name="ids[]" value="{{ $taxonomy->id }}"></td>
+                            <td>{{ $taxonomy->id }}</td>
+                            <td>{{ $taxonomy->name }}</td>
+                            <td>{{ $taxonomy->type }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <br>
+            <input type="submit" value="削除">
+        </form>
+    </div>
+</body>
+</html>

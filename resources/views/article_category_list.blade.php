6588e478f147b0f71fbf7e74596e2bfc2c57ee72
aoyama-n <57561847+aoyama-n@users.noreply.github.com>
2020-01-27 15:17:32 +0900

    記事閲覧のbladeの作成

---
+++ resources/views/article_category_list.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+
+    <meta charset="utf-8">
+    <title>カテゴリー一覧</title>
+
+</head>
+
+<body>
+    <div class="flex-center position-ref">
+        <h1>ブログカテゴリー一覧</h1>
---
+++ resources/views/top.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+
+    <meta charset="utf-8">
+    <title>トップページ</title>
+
+</head>
+
+<body>
+	<h1>トップページ</h1>
+	<a href ="blog_list">記事一覧</a>
+	<br>
+	<a href ="article_category_list">カテゴリー一覧へ</a>
+	<br>
+</body>
+
+</html>

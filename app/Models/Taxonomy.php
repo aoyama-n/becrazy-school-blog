56046703e35928b502e17b8b1735d13481c0e9b7
aoyama-n <57561847+aoyama-n@users.noreply.github.com>
2020-01-29 12:55:11 +0900

    論理削除機能を追加

--- app/Models/Taxonomy.php
+++ app/Models/Taxonomy.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Taxonomy extends Model
 {
-    //
+    //テーブル名の指定
     protected $table = 'taxonomy';
+    //論理削除
+    use SoftDeletes;
 }
